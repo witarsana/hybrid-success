3ace478752d98129980c4cd56fa63ab77c71759f
Witarsana <made.witarsana@gmail.com>
2021-02-12 17:51:16 +0800

    Modify to hybrid application

---
+++ src/app-ajs/phone-list/phone-list.component.ts
@@ -0,0 +1,13 @@
+"use strict";
+
+// Register `phoneList` component, along with its associated controller and template
+angular.module("phoneList").component("phoneList", {
+  templateUrl: "./phone-list.template.html",
+  controller: [
+    "Phone",
+    function PhoneListController(Phone) {
+      this.phones = Phone.query();
+      this.orderProp = "age";
+    },
+  ],
+});
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { UpgradeModule } from "@angular/upgrade/static";
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+// Import these globally to bring in their @types
+import "angular";
+import "angular-animate";
+import "angular-resource";
+import "angular-route";
+
+// And import our AngularJS module
+import "./app-ajs";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((platformRef) => {
+    // Use the upgrade module to bootstrap the hybrid
+    const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+    upgrade.bootstrap(document.documentElement, ["phonecatApp"]);
+  });
